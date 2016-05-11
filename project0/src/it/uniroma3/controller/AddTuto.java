cdc5e2193d46e2af0d7476defd2d81c37644fc21
GiulianoPes <giulianopes@gmail.com>
2016-05-11 16:57:24 +0200

    AddTuto,TutoDao

--- project0/src/it/uniroma3/controller/AddTuto.java
+++ project0/src/it/uniroma3/controller/AddTuto.java
@@ -1,6 +1,7 @@
 package it.uniroma3.controller;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -8,33 +9,40 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import it.uniroma3.dialog.Dialog;
+import it.uniroma3.model.Categoria;
+import it.uniroma3.model.Tuto;
+import it.uniroma3.model.Utente;
+import it.uniroma3.persistence.TutoDAO;
+
 @WebServlet("/addTuto")
-public class AddTuto {
-	public void doPost(HttpServletRequest request,HttpServletResponse response)throws ServletException, IOException{
+public class AddTuto extends HttpServlet {
+	protected void doPost(HttpServletRequest request,HttpServletResponse response)throws ServletException, IOException{
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("users-unit");
 		TutoDAO tutoDao = new TutoDAO(emf);		
 		
 		//Ricevo l'utente dalla sessione
 		HttpSession session = request.getSession();
-		Utente utente = session.getAttribute("Utente");
+		Utente owner = (Utente) session.getAttribute("owner");
 		//Ricevo i parametri per l'inserimento di un nuovo Tuto		 
-		Categoria categoria = request.getParameter("");
-		String text = request.getParameter("");
-		Tuto tuto = new Tuto(,,);
+		String text = request.getParameter("text");
+		//Date creationtime = request.getParameter("creationtime");
+		Tuto tuto = new Tuto(owner, text, new Date(System.currentTimeMillis()));
 		
 		//Salvo il tutto nel Db
 		tutoDao.save(tuto);
 		
 		//Invio su showTuto.jsp il riepilogo del tuto
 		request.setAttribute("Tuto",tuto);
-		String nextPage = "/showTuto.jsp";
+		String nextPage = "/showTuto.jsp";		
 		
-		
+		request.setAttribute("Dialog", new Dialog("Hai inserito il tuo tuto"));
 		
 		ServletContext application = getServletContext();		
 		RequestDispatcher rd = application.getRequestDispatcher(nextPage);		
