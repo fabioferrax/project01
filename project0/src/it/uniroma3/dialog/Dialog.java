666c515acddd301fe1fcb12d67620958e3506e0b
GiulianoPes <giulianopes@gmail.com>
2016-05-11 13:42:05 +0200

    Dialog

---
+++ project0/src/it/uniroma3/dialog/Dialog.java
@@ -0,0 +1,21 @@
+
+package it.uniroma3.dialog;
+
+public class Dialog{
+	String message;
+	
+	public Dialog(){}
+	
+	public Dialog(String message){
+		this.message=message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
